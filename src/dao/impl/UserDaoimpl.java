6f72c7ecaa5d2a2c74bd9205053cb8083dd5aa73
田先生 <@DESKTOP-DTLOU6V.lan>
2020-03-02 16:44:14 +0800

    修改了dao中的操作

--- src/dao/impl/UserDaoimpl.java
+++ src/dao/impl/UserDaoimpl.java
@@ -22,4 +22,9 @@ public class UserDaoimpl implements UserDao {
 		}
 	}
 
+	@Override
+	public void update(User user) {
+		System.out.println("执行修改用户信息的操作！");
+	}
+
 }
--- src/servlet/UserServlet.java
+++ src/servlet/UserServlet.java
@@ -17,9 +17,10 @@ import service.impl.UserServiceimpl;
  * @author 田先生
  *
  */
-@WebServlet("")
+//设置访问的URL的信息
+@WebServlet("/Add")
 public class UserServlet extends HttpServlet {
-
+	//业务层
 	@Override
 	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
 		// TODO Auto-generated method stub
@@ -28,9 +29,18 @@ public class UserServlet extends HttpServlet {
 
 	@Override
 	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		//设置一个请求数据的编码
+		req.setCharacterEncoding("UTF-8");
 		// 在servlet中去访问service中的数据
 		UserService us = new UserServiceimpl();
 		User user = new User();
+		//获得页面上的数据
+		String name = req.getParameter("name");
+		// 页面上获得的数据所有都是String类型，而年龄是int
+		//所有我们需要对着数据进行强制(包装类)
+		int age = Integer.parseInt(req.getParameter("age"));
+		user.setName("张三");
+		user.setAge(20);
 		us.save(user);
 	}
 
